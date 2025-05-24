44f7d0b0ba635f6f3f47d984280c3baa3c9081b9
RIFAT ISLAM <97521881+Rifat010@users.noreply.github.com>
2025-05-24 13:42:27 +0000

    data types

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,25 @@
+// ============== Javascript Data Types ========
+
+/* 
+(1) String;
+(2) Number
+(3) Boolean
+(4) BigInt
+(5) Object
+(6) Symble
+(7) Undefine
+(8) Null
+*/
+
+let string = 'String';
+let number = 12345;
+let boolean = true / false;
+let bigInt = 10n;
+let object = {userName : 'rifat islam', password : 123};
+let symble = symble('token');
+let undefine = undefined;
+let nul = null;
+
+
+// there are 8 types of data type in javascript
+console.log(bigInt)
