57025ab69ecf2b524404a152b019710f9da1443a
RIFAT ISLAM <97521881+Rifat010@users.noreply.github.com>
2025-05-24 11:45:16 +0000

    onother update

---
+++ 01_basic/test.js
@@ -0,0 +1,10 @@
+let x = 'hello world!'
+console.log(x);
+let a = 10;
+let b = 20;
+const sum = a+b;
+
+console.log(sum);
+
+const randomMath = Math.floor(Math.random()*10) + 1;
+console.log(randomMath);
